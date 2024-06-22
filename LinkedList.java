1662b690de31f2e23e6f05f0de11c1de6b620142
liangalv <alvin.liang@mail.utoronto.ca>
2024-06-22 10:48:56 -0400

    implemented search function

--- LinkedList.java
+++ LinkedList.java
@@ -18,6 +18,31 @@ public class LinkedList{
     //Insert O(n)
     //Deletion O(n)
 
+    //Returns whether Item is in LinkedList
+    public boolean search(ListNode listNode){
+        //This line gets the value we are searching for
+        int valToSearch = listNode.getValue(); 
+        //This sets the head, i.e (that is) the beginning of the list
+        ListNode currNode = this.firstNode;
+        //If we find a null node this means that we are at the end of the list
+        //Therefore we will exit this while loop
+        while(Objects.nonNull(currNode)){
+            if (valToSearch == currNode.getValue()){
+                return true;
+                }
+            //variable currNode is now referring to the next node
+            currNode = currNode.getNext();
+            }
+        return false;
+        }
+    }
+    public void insert(ListNode listNode){
+
+    }
+    public void deletion(int index){
+
+    }
+
     /* HELPERS */
 
     private ListNode locateNode(int valueToFind){
@@ -52,7 +77,7 @@ private class ListNode{
         this.value = value
     }
     public void setNextNode(ListNode node){
-        this.next = 
+        this.next = node;
     }
     /*GETTERS */
     public ListNode getNext(){
